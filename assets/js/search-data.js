a179e441097b96f95c167fa503cca7f3a727d424
harshitakukreja <32929915+harshitakukreja@users.noreply.github.com>
2025-12-01 04:39:10 +0000

    Deploying to gh-pages from @ harshitakukreja/harshitakukreja.github.io@7938cae6a7ec43b56b1b9ff4d102f09f44b7cad2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Details about my background.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -473,7 +473,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/harshitakukreka_resume.pdf", "_blank");
+          window.open("/assets/pdf/harshitakukreja_resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
